9573dc16d6b5c037e6f747766c4592972f01da3c
Taha Hocine <tahahocinearrif83@gmail.com>
2026-03-25 00:36:08 -0700

    firstcommit

--- lib/main.dart
+++ lib/main.dart
@@ -9,108 +9,157 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Facebook',
       debugShowCheckedModeBanner: false,
-
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-                    onPressed: () {},
-                    backgroundColor: Colors.blue,
-                    child: Icon(
-                            Icons.add,
-                            color: Colors.white,
-                              ),
-          ),
-        
-
         appBar: AppBar(
-          title: Text('Facebook'),
+          elevation: 10,
+          title: Text(
+            "Facebook",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w700,
+              color: Colors.blue,
+            ),
+          ),
           centerTitle: true,
-          backgroundColor: Colors.white,
           leading: IconButton(
-            icon: Icon(Icons.menu),
             onPressed: () {},
-            color: Colors.blue,
+            icon: Icon(Icons.menu, size: 30, color: Colors.blue),
           ),
           actions: [
             IconButton(
-              icon: Icon(Icons.search),
               onPressed: () {},
-              color: Colors.blue,
+              icon: Icon(Icons.message, size: 30, color: Colors.blue),
             ),
             IconButton(
-              icon: Icon(Icons.message),
               onPressed: () {},
-              color: Colors.blue,
+              icon: Icon(Icons.search, size: 30, color: Colors.blue),
             ),
           ],
         ),
-        body:  SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(50,0 , 0,0 ),
-          child :Container(
-            color : Colors.grey,
-            child: Column(
-              
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-            
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 7, 255, 48),
-                    border: Border.all(color: Colors.blue, width: 2),
-                    
-                  ),
-                  height: 140,
-                  width: 200,
-                  child: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)
-                  
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 325,
+                width: 600,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  "Matjari.shop",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
-                SizedBox(height: 280),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 7, 32, 255),
-                    border: Border.all(color: Colors.blue, width: 2),
-                    shape :BoxShape.circle,
-                  ),
-                  height: 100,
-                  width: 100,
-                  child: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)
-                  
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      height: 200,
+                      width: 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        "Matjari.shop",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      height: 200,
+                      width: 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        "Matjari.shop",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      height: 200,
+                      width: 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        "Matjari.shop",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      height: 200,
+                      width: 150,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        "Matjari.shop",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 500,
+                width: 600,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  "Matjari.shop",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
-                SizedBox(height: 280),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 255, 7, 123),
-                    border: Border.all(color: Colors.blue, width: 2),
-                    shape :BoxShape.circle,
-                  ),
-                  height: 100,
-                  width: 100,
-                  child: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)
-                  
-                  ),
-                ),
-                SizedBox(height: 280),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 7, 255, 48),
-                    border: Border.all(color: Colors.blue, width: 2),
-                    shape :BoxShape.circle,
-                  ),
-                  height: 100,
-                  width: 100,
-                  child: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)
-                  
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-        ),
-      );
+      ),
+    );
   }
 }
